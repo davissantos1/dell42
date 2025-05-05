5a6329186b033f3fa10f31a509aaf53fbdd25e3c
Davi Santos <davisimoesdossantos1@gmail.com>
2025-05-05 12:42:18 -0300

    finished ft_atoi_base

--- C/C04/ex05/ft_atoi_base.c
+++ C/C04/ex05/ft_atoi_base.c
@@ -25,17 +25,72 @@ int	ft_base_check(char *base)
 	}
 	return (1);
 }
-int	ft_atoi(char *str, char *base, int base_num)
+int	ft_base_index(char ch, char *base)
+{
+	int index;
+	
+	index = 0;
+	while (base[index] != ch)
+		index++;
+	return (index);
+}
+int	ft_str_check(char *str, char* base, int start)
 {
 	int j;
-	int nb;
+	int flag;
+	
+	j = 0;
+	flag = 0;
+	while (base[j])
+	{
+		if (str[start] == base[j])
+			flag = 1;
+		j++;
+	}
+	return (flag);
+}
+int	ft_convert_int(char *str, char *base, int base_num, int i)
+{
+	int j;
+	int start;
+	int result;
+	
+	j = 0;
+	start = i;
+	result = 0;
+	while (base[j])
+	{
+		while ((str[i] == base[j]) && str[i])
+		{
+			i++;
+			j = 0;
+		}
+		j++;
+	}
+	if (!ft_str_check(str, base, start))
+		return (0);
+	while (start < i)
+	{
+		result = result * base_num + ft_base_index(str[start], base);
+		start++;
+	}
+	return (result);
+}
+int	ft_atoi_base(char *str, char *base)
+{
+	int i;
 	int signs;
 	int result;
+	int base_num;
 
 	i = 0;
 	result = 0;
-	nb = 1;
 	signs = 1;
+	base_num = 0;
+	if (!(ft_base_check(base)))
+		return (0);
+	while (base[base_num])
+		base_num++;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	while (str[i] == '+' || str[i] == '-')
@@ -44,30 +99,18 @@ int	ft_atoi(char *str, char *base, int base_num)
 			signs *= -1;
 		i++;
 	}
-	while (nb)
-	{
-		j = 0;
-		while (base[j])
-		{
-			if (str[i] != base[j])
-				nb = 0;
-			else
-				result = result * basenum + (j - 1);
-			j++
-		}
-		i++;
-
-	}
+	result = ft_convert_int(str, base, base_num, i);
+	if (!result)
+		return (0);
 	return (signs * result);
 }
-int	ft_atoi_base(char *str, char *base);
+/*
+int	main(void)
 {
-	int base_num;
-
-	base_num = 0;
-	if (!(ft_base_check(base)) || (ft_str_check(str, base))
-		return (0);
-	while (base[base_num])
-		base_num++;
-	ft_atoi(str, base, base_num);
+	char data[] = "  ---+--+101010ab567";
+	char base[] = "01";
+	
+	printf("%d\n", ft_atoi_base(data, base));
+	return (0);
 }
+*/
